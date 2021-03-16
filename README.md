c2eef32888e871d77b7427a72cda4185577dd861
AleConco <aleconco@mymail>
2021-03-17 00:35:58 +0100

    add working configuration

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# gcp-auto-tag
+# GCP Autolaber Reloaded 2021 - Autolabel object, adapted on GCP 03/2021
+
+Function che aggiunge custom label (es. label acronimo) alle istanze e relativi dischi quando intercetta una creazione di istanza compute engine.
+
+Adattato codice e configurazioni da [Orginal version](https://github.com/doitintl/gcp-auto-tag), attualmente non piu funzionanti per passaggio da Stackdriver a Cloud Log
+
+Original Idea, references:
+
+- [Autolabel object for](https://blog.doit-intl.com/automatically-label-google-cloud-compute-engine-instances-and-disks-upon-creation-5d1245f361c1)
+
+
+Descrizione scenario:
+
+- in fase di creazione compute instance vengono generati di log di audit
+
+- tramite un sink sono catturati ed inseriti in una coda pub/sub.
+
+- il sink è usato dalla function python in questo repo
+
+DONE:
+
+- Change Skink syntax (add filter for a custom project) for new cloudlog
+- fix python code to new json scheme
+- Add label "acronimo: skrt0" (attenzione, label minuscole)
+
+TODO:
+
+- imparare a far miglior debug o a versionare function
+
+
+
+## GCP Configurations Step
+
+- Create roles and serviceaccounts
+
+```bash
+# Replace $MY_PROJECT with your project id
+
+SERVICE_ACCOUNT=autolabel_instances
+MY_PROJECT=ocp-isolated
+
+gcloud iam roles create $SERVICE_ACCOUNT  --title=autolabel-instances --project $MY_PROJECT --description='cloud function service account to add acronimo label' --permissions=compute.disks.get,compute.disks.setLabels,compute.instances.get,compute.instances.setLabels --stage=GA
+gcloud iam service-accounts create autolabel-instances
+
+gcloud projects add-iam-policy-binding $MY_PROJECT --member serviceAccount:autolabel-instances@$MY_PROJECT.iam.gserviceaccount.com --role projects/$MY_PROJECT/roles/$SERVICE_ACCOUNT
+gcloud projects add-iam-policy-binding $MY_PROJECT --member serviceAccount:autolabel-instances@$MY_PROJECT.iam.gserviceaccount.com --role roles/logging.logWriter
+```
+
+- Creare sink da usare, adeguato con Cloud Log (son cambiati i json da Stackdriver!!) 03/2021
+
+- formato sink (è cambiato formato json del logging)  [router](https://console.cloud.google.com/logs/router?project=ocp-isolated)
+
+object-to-label-sink -> importante avere log univoco, importante operation.first="true" !!
+
+```conf
+resource.type="gce_instance"
+resource.labels.project_id="ocp-isolated"
+protoPayload.methodName="beta.compute.instances.insert"
+operation.first="true"
+```
+
+## Misc
+
+setting authentication outside cloudshell or function
+
+```bash
+export GOOGLE_APPLICATION_CREDENTIALS="/home/u357595/.gcp/osServiceAccount.json"
+```
+
+```bash
+gcloud beta compute --project=ocp-isolated instances create vmtolabel006 --zone=europe-west3-c --machine-type=f1-micro --subnet=subnet-eu-west-3 --no-address --maintenance-policy=MIGRATE --service-account=351803534639-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --image=debian-10-buster-v20210217 --image-project=debian-cloud --boot-disk-size=10GB --boot-disk-type=pd-balanced --boot-disk-device-name=vmtolabel005 --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any
+```
+
--- main.py
+++ main.py
@@ -4,17 +4,20 @@ import base64
 import json
 from google.auth import compute_engine
 
+# qui mi serve per recupere le credenziali dal serviceaccount
 credentials = compute_engine.Credentials()
 compute = googleapiclient.discovery.build('compute', 'v1')
 
-
-def tag_instance(instance: str, project: str, zone: str, creator_email: str):
+def tag_instance(instance: str, project: str, zone: str):
     # tag (label) the instance and return a list of the disk volumes
     instance_information = compute.instances().get(project=project, zone=zone, instance=instance).execute()
     instance_disks_list = [disk['deviceName'] for disk in instance_information['disks']]
+
+    # recupera le label precedenti in modo da andare in append
     instance_fingerprint = instance_information['labelFingerprint']
-    instance_labels = {'labels': {'created-by': creator_email}, 'labelFingerprint': instance_fingerprint}
-    request = compute.instances().setLabels(project=project, zone=zone, instance=instance, body=instance_labels)
+    # https://cloud.google.com/compute/docs/reference/rest/v1/instances/setLabels#request-body
+    instances_set_labels_request_body = {'labels': {'acronimo': 'skrt0'}, 'labelFingerprint': instance_fingerprint}
+    request = compute.instances().setLabels(project=project, zone=zone, instance=instance, body=instances_set_labels_request_body)
     try:
         request.execute()
         return {'status': True, 'instance_disks_list': instance_disks_list}
@@ -23,7 +26,7 @@ def tag_instance(instance: str, project: str, zone: str, creator_email: str):
         return {'status': False, instance_disks_list: []}
 
 
-def tag_disks(disks_list: list, project: str, zone: str, instance_name, creator_email: str):
+def tag_disks(disks_list: list, project: str, zone: str, instance_name):
     # tag a volume from the instance volume list
     for disk in disks_list:
         logging.info(f'going to tag disk {disk}')
@@ -34,32 +37,38 @@ def tag_disks(disks_list: list, project: str, zone: str, instance_name, creator_
             disk_data = compute.disks().get(project=project, zone=zone, disk=instance_name).execute()
             disk = instance_name
         disk_fingerprint = disk_data['labelFingerprint']
-        disk_labels = {'labels': {'created-by': creator_email, 'instance': instance_name},
-                       'labelFingerprint': disk_fingerprint}
+        disk_set_labels_request_body = {'labels': {'acronimo': 'skrt0', 'instance': instance_name}, 'labelFingerprint': disk_fingerprint}
+
         try:
-            compute.disks().setLabels(project=project, zone=zone, resource=disk, body=disk_labels).execute()
+            compute.disks().setLabels(project=project, zone=zone, resource=disk, body=disk_set_labels_request_body).execute()
         except Exception as e:
             print(str(e))
     return True
 
-
 def hello_pubsub(event, context):
     # parse the pubsub event
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    print('Dump del messaggio ' + pubsub_message)
     pubsub_message = json.loads(base64.b64decode(event['data']).decode('utf-8'))
+    
     # pubsub variables
-    user_email = pubsub_message['jsonPayload']['actor']['user'].replace('@', '_', ).replace('.', '-')
-    instance_zone = pubsub_message['jsonPayload']['operation']['zone']
-    instance_name = pubsub_message['jsonPayload']['resource']['name']
     project_id = pubsub_message['resource']['labels']['project_id']
+    instance_name = pubsub_message['protoPayload']['request']['name']
+    # è una lista di zone, anche se con un elemento, da approfondire
+    instance_zone = pubsub_message['protoPayload']['resourceLocation']['currentLocations'][0]
+    # attenzione che la mail non è un valore valido per le label
+    user_email = pubsub_message['protoPayload']['authenticationInfo']['principalEmail']
 
-    logging.info(f'new instance created, going to tag instance {instance_name}')
-    # tag the instance
-    instance_tag = tag_instance(instance_name, project_id, instance_zone, user_email)
+    print("A new Compute instance is been created by " + user_email + " on project: " + project_id + ", instance name:" + instance_name + ", on zone: " + instance_zone + ". We need a label :) ")
+    logging.info(f'new instance created by {user_email}, going to tag instance {instance_name} on project {project_id} and zone {instance_zone}')
+
+    instance_tag = tag_instance(instance_name, project_id, instance_zone)
     
     # if instance tag was successful and the instance volume list exists
     if instance_tag and instance_tag['instance_disks_list']:
         disks_list = instance_tag['instance_disks_list']
         # tag volumes
-        disks_tag = tag_disks(disks_list, project_id, instance_zone, instance_name, user_email)
+        disks_tag = tag_disks(disks_list, project_id, instance_zone, instance_name)
         if disks_tag:
             return True
+    logging.info(f'Instance {instance_name} labeled')
